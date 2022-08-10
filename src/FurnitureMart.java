89faed7b39c242fbb9b52b8916a27f38823d6969
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-08-10 09:04:21 +0530

    Sprint1

---
+++ src/FurnitureMart.java
@@ -0,0 +1,10 @@
+public class FurnitureMart {
+    String plasticFurniture;
+    String furnitureType;
+    double discount;
+
+    public double outdoorFurniture(double priceOfOutdoorFurniture){
+        discount=priceOfOutdoorFurniture*0.05;
+        return discount;
+    }
+}
